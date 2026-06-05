55b055e97e07bf29b52a612ef96b21cc23eed1e5
lhj-7777777 <1453782818@qq.com>
2026-06-05 20:30:12 +0800

    Update page.tsx

--- src/app/live2d/page.tsx
+++ src/app/live2d/page.tsx
@@ -5,7 +5,7 @@ import Live2DViewer from './live2d-viewer'
 export default function Live2DPage() {
 	return (
 		<div className='flex h-full items-center justify-center py-8'>
-			<Live2DViewer />
+			{/* <Live2DViewer /> 模型不显示 */}
 		</div>
 	)
 }
